da18bbe7778015a13b1cad1a48f19b3b4542a17d
Ash1sh-B <156790016+Ash1sh-B@users.noreply.github.com>
2025-03-10 20:53:04 +0000

    Update Program.cs

--- RecordsStructureTask1/Program.cs
+++ RecordsStructureTask1/Program.cs
@@ -22,10 +22,11 @@
         {
             Film hulk = new Film("Hulk", "12A", 2005, false);
             Film ironMan = new Film("Iron Man","12A",2008,false);
-            Film antMan = new Film("Ant-Man", "12A", 2015, false);
+            Film antMan = new Film("Ant-Man", "12A", 2015-, false);
             Film[] filmCollection = new Film[] { antMan,hulk,ironMan };
             int year = 0;
             int position = 0;
+            Console.Writeline(antman);
 
             //  Write the code to do the following
             // Loop through the array of films and check for the newest film
